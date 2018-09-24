57e05b07a49bedae7679821dbf5052e1d4bd08de
abe546 <abe546@live.com>
2018-09-24 00:43:08 -0400

    Added more constant vars, use of methods, dynamic elements, and organized items.

--- main/src/functions.js
+++ main/src/functions.js
@@ -1,30 +1,40 @@
+//Constants
+var BACK = "back";
+var CURRENT = "current";
+var ON = "on";
+var OFF = "off";
+var RANDOM_URL = "https://old.reddit.com/random/";
 
 window.onload=function start()
 {
-  console.log("Hello world")
-
+  console.log("Hello world");
+  //On by default
+  if(isOn()!=0 && isOn()!=1)
+  {
+  localStorage.setItem(ON,1);
+}
+  //Do this so that current is set, and then
+  //back can be set to what current is also
+  setCurrentSite(window.location.href);
+  updateValues(window.location.href);
   //Adds on/off button to top of webpage
   prependOnOffButton();
 
-   var write = document.getElementById("back");
+	var $write = $("<br><a target=\"_blank\" href='https://www.old.reddit.com"+localStorage.getItem(BACK)+"'>Click here, or press the 'b' key, to go back to the last subreddit shown. ("+ localStorage.getItem(BACK) +") </a></br>");
 
-   if(localStorage.getItem("back") == null || localStorage.getItem("back") == "null")
-   {
-	   localStorage.setItem("back","empty");
-   }
+ $write.prependTo($("body"));
 
-	write.innerHTML=""
-	write.innerHTML += "<a target=\"_blank\" href='https://www.reddit.com"+localStorage.getItem("back")+"'>Click here, or press the 'b' key, to go back to the last subreddit shown. ("+ localStorage.getItem("back") +") </a>";
-}
-;
-function oldResult(variable)
+ assignOnOffButton();
+};
+
+function updateValues(variable)
 {
 
 	//console.log("In old");
-	localStorage.setItem("back",localStorage.getItem("current"));
-	localStorage.setItem("current",variable);
-	console.log("Old result back : "+localStorage.getItem("back"));
-	console.log("Current : "+localStorage.getItem("current"));
+	localStorage.setItem(BACK,localStorage.getItem(CURRENT));
+	localStorage.setItem(CURRENT,variable);
+	console.log("Old result back : "+localStorage.getItem(BACK));
+	console.log("Current : "+localStorage.getItem(CURRENT));
 
 
 	/* $.ajax({
@@ -40,31 +50,51 @@ function oldResult(variable)
 
 	});*/
 
-
-
-
-
 }
 
 $(document).keydown(function(e){
  console.log(e.keyCode);
 
+ //r key
  if(e.keyCode==82)
  {
-	 window.location.replace("https://web.njit.edu/~ic56/random/");
+   if(isOn()==1)
+   {
+	 window.location.replace(RANDOM_URL);
+   updateValues(window.location.href);
+   }
  }
+ //b key
  else if(e.keyCode==66)
  {
-	 //window.location.replace("https://web.njit.edu/~ic56/random/");
-     window.open("https://www.reddit.com"+localStorage.getItem("back"), '_blank');
+     if(isOn()==1)
+     {
+     window.open(localStorage.getItem(BACK), '_blank');
+     }
  }
 
 });
 
-function getOld()
+function setOldSite(site)
 {
-	console.log(localStorage.getItem("back"));
-	return localStorage.getItem("back");
+  localStorage.setItem(BACK,site);
+}
+
+function setCurrentSite(site)
+{
+  localStorage.setItem(CURRENT,site);
+}
+
+function getOldSite()
+{
+	console.log(localStorage.getItem(BACK));
+	return localStorage.getItem(BACK);
+}
+
+function getCurrentSite()
+{
+  console.log(localStorage.getItem(CURRENT));
+  return localStorage.getItem(CURRENT);
 }
 
 /*Prepends an on/off button to top of webpage (before body)
@@ -74,24 +104,41 @@ function getOld()
 */
 function prependOnOffButton()
 {
-  var $input = $('<input type="button" value="on/off" id="onOff" />');
+  var $input;
+
+if(isOn()==1)
+{
+  $input = $('<input type="button" value="on" id="onOff" />');
+}
+else{
+  $input = $('<input type="button" value="off" id="onOff" />');
+}
+
      $input.prependTo($("body"));
+}
+
+function  isOn()
+{
+  return localStorage.getItem(ON);
 }
 
 //Keeps track of value of on or off switch
 function assignOnOffButton()
 {
+  console.log("BEGINNING OF METHOD"+isOn());
    $("#onOff").click(function(){
-   if(localStorage.getItem("on")=="null" || localStorage.getItem("on") == null || localStorage.getItem("on") == 0 )
+   if(localStorage.getItem(ON) == 0 )
   {
-    localStorage.setItem("on",1);
+    $("#onOff").val(ON);
+    localStorage.setItem(ON,1);
   }
-  else if(localStorage.getItem("on") == 1)
+  else if(localStorage.getItem(ON) == 1)
   {
-    localStorage.setItem("on",0)
+      $("#onOff").val(OFF);
+    localStorage.setItem(ON,0);
   }
-
+console.log("END OF METHOD"+isOn());
   return;
-}
+});
 
 }
